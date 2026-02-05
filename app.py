99980e97ae487e46dd98c9a805c48f25fe4fc53e
Prima Ammaray Baroo <primaammaray@gmail.com>
2026-02-05 21:18:48 +0700

    .

--- app.py
+++ app.py
@@ -187,7 +187,7 @@ def generate_pdf(df_source, year, month):
             
             x, y = pdf.get_x(), pdf.get_y()
             pdf.cell(col_day, 10, "", 1, 0, 'C', fill=fill)
-            pdf.set_xy(x, y+1); pdf.set_font("Arial",'',3); pdf.multi_cell(col_day, 3, txt, 0, 'C')
+            pdf.set_xy(x, y+1); pdf.set_font("Arial",'',4); pdf.multi_cell(col_day, 4, txt, 0, 'C')
             pdf.set_xy(x+col_day, y); pdf.set_font("Arial",'',6)
         
         # Kolom Summary di Kanan
